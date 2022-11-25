4b34bbd147018f53242095db07a518832837fa2f
StarPandaBeg <kshmurakov@mail.ru>
2022-11-25 19:42:39 +0300

    Initial

---
+++ CattyLED.ino
@@ -0,0 +1,12 @@
+#define CATTY_VERSION "CattyLED v0.0.1 (27.10.2022) by StarPanda"
+
+#include "Locale.h"
+#include "Configuration.h"
+#include "Libraries.h"
+#include "Global.h"
+
+void setup() {
+}
+
+void loop() {
+}
